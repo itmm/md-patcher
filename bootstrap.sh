9692fe227547c14989c004ddae1b1be006886d51
Timm Knape <timm@knp.de>
2024-03-12 22:35:16 +0100

    refactoring

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-g++ -std=c++17 \
-  -I solid-require/include \
-  -I line-reader/include \
-  -I lazy-write/include \
-  md-patcher.cpp \
-  solid-require/solid/require.cpp \
-  lazy-write/lazy-write.cpp \
-  line-reader/line-reader.cpp \
+c++ -std=c++17 \
+  -I solid-require/include -I line-reader/include -I lazy-write/include \
+  md-patcher.cpp solid-require/solid/require.cpp \
+  lazy-write/lazy-write.cpp line-reader/line-reader.cpp \
   -o mdp
